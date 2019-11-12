c1b84a9f62027e84722bc4acbf511fd5f48d3e53
Cooper Higgins <cooperhiggins@gmail.com>
2019-11-12 13:29:51 -0500

    changed carousel css and navbar css

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
 console.log('Welcome To Heaing Aids Chesapeake');
+
+$(document).ready(function(){
+    $(".dropdown, .btn-group").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+});    
