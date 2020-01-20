7e927b0a173c111c5cf9a33d73cc20b735f88fed
Cooper Higgins <cooperhiggins@gmail.com>
2020-01-20 15:47:20 -0500

    deleted unused scripts logging error from main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,2 @@
 console.log('Welcome To Heaing Aids Chesapeake');
 
-$(document).ready(function(){
-    $(".dropdown, .btn-group").hover(function(){
-        var dropdownMenu = $(this).children(".dropdown-menu");
-        if(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("open");
-        }
-        else(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("open");
-        }
-    });
-});   
-
-$(document).ready(function(){
-    $(".dropdown, .btn-group").onClick(function(){
-        var dropdownMenu = $(this).children(".dropdown-menu");
-        if(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("open");
-        }
-        else(dropdownMenu.is(":visible")){
-            dropdownMenu.parent().toggleClass("close");
-        }
-    });
-});    
