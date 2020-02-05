8f35ffa66e902cf1fd5aece15517c048a0384fda
Cooper Higgins <cooperhiggins@gmail.com>
2020-02-05 17:11:32 -0500

    created AMP Page and added script to index.html to load chesapeke-hearing-aids-amp.html on mobile devices

---
+++ scripts/nav.js
@@ -0,0 +1,10 @@
+document.getElementById('a#icon').addEventListener("onclick", myFunction);
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
